293b7a33d9f11d41994230032385a093b17aca98
aalonee <alone@evolution-project.online>
2023-10-01 23:12:03 +0300

    Добавьте файлы проекта.

---
+++ TK_Warning/Config.cs
@@ -0,0 +1,30 @@
+﻿using Exiled.API.Interfaces;
+using System.ComponentModel;
+
+namespace Template
+{
+    public class Config : IConfig
+    {
+        public bool IsEnabled { get; set; } = true;
+
+        public bool Debug { get; set; } = false;
+
+        [Description("The message to show to the teamkiller (uses ShotDuration)")]
+        public string AttackerShooting { get; set; } = "<color=red>YOU'RE SHOOTING AT YOUR TEAMMATE <color=orange>%target%<color=red>!</color>";
+
+        [Description("The message to show to the target (uses ShotDuration)")]
+        public string TargetShot { get; set; } = "<color=red>YOU WERE SHOT BY <color=orange>%attacker%<color=red>!</color>";
+
+        [Description("The message to show to the teamkiller after killing the teammate (uses KillDuration)")]
+        public string AttackerKilling { get; set; } = "<color=red>YOU TEAMKILLED YOUR TEAMMATE <color=orange>%target%<color=red>!</color>";
+
+        [Description("The message to show to the targer after getting killed by teamkiller (uses KillDuration)")]
+        public string TargetKilled { get; set; } = "<color=red>YOU WERE TEAMKILLED BY <color=orange>%attacker%<color=red>!</color>";
+
+        [Description("Duration of the hint")]
+        public float ShotDuration { get; set; } = 0.5f;
+
+        [Description("Duration of the hint")]
+        public float KillDuration { get; set; } = 3.0f;
+    }
+}
---
+++ TK_Warning/Plugin.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Linq;
+using Exiled.API.Enums;
+using Exiled.API.Extensions;
+using Exiled.API.Features;
+using Exiled.Events.Commands.Reload;
+using Exiled.Events.EventArgs;
+using Exiled.Events.EventArgs.Player;
+using PluginAPI.Events;
+
+namespace Template
+{
+    public sealed class Plugin : Plugin<Config>
+    {
+        public override PluginPriority Priority { get; } = PluginPriority.Default;
+        public override string Name { get; } = "TeamKill Warning";
+
+        public override string Author { get; } = "alone";
+
+        public override string Prefix { get; } = "TKWarning";
+
+        public override void OnEnabled()
+        {
+            base.OnEnabled();
+            Exiled.Events.Handlers.Player.Hurting += OnHurt;
+            Exiled.Events.Handlers.Player.Dying += OnTK;
+        }
+
+        public override void OnDisabled()
+        {
+            base.OnDisabled();
+            Exiled.Events.Handlers.Player.Hurting -= OnHurt;
+            Exiled.Events.Handlers.Player.Dying -= OnTK;
+        }
+
+        public void OnHurt(HurtingEventArgs ev)
+        {
+            if (ev.Attacker != null && ev.Player != null && ev.Attacker != ev.Player)
+            {
+                if (ev.Attacker.Role.Team == ev.Player.Role.Team)
+                {
+                    ev.Attacker.ShowHint(Config.AttackerShooting.Replace("%target%", ev.Player.Nickname), Config.ShotDuration);
+                    ev.Player.ShowHint(Config.TargetShot.Replace("%attacker%", ev.Attacker.Nickname), Config.ShotDuration);
+                }
+            }
+        }
+
+        public void OnTK(DyingEventArgs ev)
+        {
+            if (ev.Attacker != null && ev.Player != null && ev.Attacker != ev.Player)
+            {
+                if (ev.Attacker.Role.Team == ev.Player.Role.Team)
+                {
+                    ev.Attacker.ShowHint(Config.AttackerKilling.Replace("%target%", ev.Player.Nickname), Config.KillDuration);
+                    ev.Player.ShowHint(Config.TargetKilled.Replace("%attacker%", ev.Attacker.Nickname), Config.KillDuration);
+                }
+            }
+        }
+    }
+}
